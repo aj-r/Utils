3c6c28beeeac5dcb60cd882f21843fe01a6c23cc
AJ Richardson <aj_r@shaw.ca>
2014-07-22 00:28:19 -0400

    Added log4net environment folder utility

---
+++ Logging/EnvironmentFolderConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using log4net.Util;
+
+namespace Utils.Logging
+{
+    public class EnvironmentFolderConverter : PatternConverter
+    {
+        protected override void Convert(TextWriter writer, object state)
+        {
+            Environment.SpecialFolder specialFolder;
+            if (!Enum.TryParse(Option, true, out specialFolder))
+                return;
+            writer.Write(Environment.GetFolderPath(specialFolder));
+        }
+    }
+}

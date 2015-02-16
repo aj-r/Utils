76a83cdd3c5b9f25b993d63a0dc35a3181e78f76
AJ Richardson <aj.ric.ar@gmail.com>
2015-02-16 09:35:38 -0700

    Added a beforePropertyChanged action to the SetValue function

--- ObjectModel/NotifyPropertyChangedBase.cs
+++ ObjectModel/NotifyPropertyChangedBase.cs
@@ -41,11 +41,43 @@ namespace Utils.ObjectModel
             OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
         }
 
+        /// <summary>
+        /// Sets the field to the specified value. If the field is NOT already equal to the value
+        /// (using the default equality comparer), then a PropertyChanged event will be raised.
+        /// </summary>
+        /// <typeparam name="T">The type of the field to set.</typeparam>
+        /// <param name="field">The field to set.</param>
+        /// <param name="value">The value to set the field to.</param>
+        /// <param name="propertyName">
+        /// The name of the property for which the PropertyChanged event will be raised.
+        /// If this parameter is omitted, its value will be set to the name of the property or method where this method is called from.
+        /// </param>
+        /// <returns>True if the property was changed; otherwise false.</returns>
         protected bool SetValue<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            return SetValue(ref field, value, null, propertyName);
+        }
+
+        /// <summary>
+        /// Sets the field to the specified value. If the field is NOT already equal to the value
+        /// (using the default equality comparer), then a PropertyChanged event will be raised.
+        /// </summary>
+        /// <typeparam name="T">The type of the field to set.</typeparam>
+        /// <param name="field">The field to set.</param>
+        /// <param name="value">The value to set the field to.</param>
+        /// <param name="beforePropertyChanged">An action to run immediately before the PropertyChanged event is fired.</param>
+        /// <param name="propertyName">
+        /// The name of the property for which the PropertyChanged event will be raised.
+        /// If this parameter is omitted, its value will be set to the name of the property or method where this method is called from.
+        /// </param>
+        /// <returns>True if the property was changed; otherwise false.</returns>
+        protected bool SetValue<T>(ref T field, T value, Action beforePropertyChanged, [CallerMemberName] string propertyName = null)
         {
             if (EqualityComparer<T>.Default.Equals(field, value))
                 return false;
             field = value;
+            if (beforePropertyChanged != null)
+                beforePropertyChanged();
             OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
             return true;
         }
